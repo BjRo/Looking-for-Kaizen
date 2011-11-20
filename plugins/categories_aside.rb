4d8e1b274a999995503be7740f2f44df1d3c47aa
Björn Rochel <bjoern@bjro.de>
2011-11-20 19:32:55 +0100

    category_list plugin now uses the category_dir setting specified in _config.yml

--- plugins/categories_aside.rb
+++ plugins/categories_aside.rb
@@ -2,13 +2,14 @@ module Jekyll
   class CategoryListTag < Liquid::Tag
     def render(context)
       html = ""
-      categories = context.registers[:site].categories.keys
+      site = context.registers[:site]
+      category_dir = site.config['category_dir']
      
       html << "<ul id='categories'>\n"
 
-      categories.sort.each do |category|
-        posts_in_category = context.registers[:site].categories[category].size
-        html << "<li><a href='/blog/categories/#{category}/'>#{category} (#{posts_in_category})</a></li>\n"
+      site.categories.keys.sort.each do |category|
+        posts_in_category = site.categories[category].size
+        html << "<li><a href='#{category_dir}/#{category}/'>#{category} (#{posts_in_category})</a></li>\n"
       end
 
       html << "</ul>\n"
